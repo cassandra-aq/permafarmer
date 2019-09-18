94c11c3e6daf7e118834f4daa7064bda7a562ccf
Cassandra_aq <cassandra.anquetil@gmail.com>
2019-09-18 16:57:34 +0200

    fixed ProductsTableSeeder

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -22,36 +22,36 @@ class ProductsTableSeeder extends Seeder
         Product::create([
             'name' => "tomates",
             'price' => 3.20,
-            'weightStocked' => 15,
-            'unitStocked' => null,
+            'weight_stocked' => 15,
+            'unity_stocked' => null,
             'weight' => 0.15
         ]);
         Product::create([
             'name' => "carottes",
             'price' => 4.20,
-            'weightStocked' => 20,
-            'unitStocked' => null,
+            'weight_stocked' => 10,
+            'unity_stocked' => null,
             'weight' => 0.1
         ]);
         Product::create([
             'name' => "courgettes",
             'price' => 2.50,
-            'weightStocked' => 10,
-            'unitStocked' => null,
+            'weight_stocked' => 15,
+            'unity_stocked' => null,
             'weight' => 0.3
         ]);
         Product::create([
             'name' => "aubergines",
             'price' => 4.80,
-            'weightStocked' => 5,
-            'unitStocked' => null,
+            'weight_stocked' => 5,
+            'unity_stocked' => null,
             'weight' => 0.2
         ]);
         Product::create([
             'name' => "pommes de terre",
             'price' => 2.10,
-            'weightStocked' => 50,
-            'unitStocked' => null,
+            'weight_stocked' => 50,
+            'unity_stocked' => null,
             'weight' => 0.15
         ]);
     }
