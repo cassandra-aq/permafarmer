645459fb2071bb2b5323b9e30f5415357ccec3c1
Cassandra_aq <cassandra.anquetil@gmail.com>
2019-09-19 11:30:15 +0200

    Added color variables

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -66,6 +66,7 @@
 <script src="https://code.jquery.com/jquery-3.3.1.min.js"
         integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 <script src="{{ asset('js/app.js') }}" defer></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 @yield('scripts')
 </body>
 </html>
